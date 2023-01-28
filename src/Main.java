51fe83d9a432f15b9ee84117bad4bf53ff97382c
Giammaria Biffi <gmb2005itabackup@gmail.com>
2023-01-28 11:07:02 +0100

    Initial class creation and first methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		Server mainServer=new Server();
+		UserInterface ui=new UserInterface();
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Server {
+/**
+*
+ * La classe Server permette di creare un server e utilizzarlo per far comunicare L'utente con il database e ottenere
+ * i dati richiesti
+ * @version 1.0
+ * @author Giammaria Biffi, Andrea Zotti, Gabriele Di Cosmo*/
+public class Server {
+	/**
+	*
+	 * L'attributo archivio crea una istanza della classe Archivio, in grado di gestire il file necessario allo
+	 * storage dei dati*/
+	static Archivio db=new Archivio();
+
+	public static void formattaDati(String nome, String cognome, String password){
+		String stringaUtente;
+		stringaUtente=nome +","+cognome+","+password;
+		db.aggiungiUtente(stringaUtente);
+	}
 }
+
